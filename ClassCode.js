6b4aac921f2dbb2cbfd348626bd3ea242090b170
bigish123 <102987684+bigish123@users.noreply.github.com>
2022-04-15 13:58:50 -0400

    Add files via upload

---
+++ ClassCode.js
@@ -0,0 +1,18 @@
+const first = "Deposit Money";
+const second = "Withdraw Money";
+
+
+const userInput = prompt("Deposit Money or Withdraw Money?");
+
+if (userInput == first) {console.log(prompt("How much would you like to deposit"));}
+else if (userInput == second) {console.log(prompt("How much would you like to withdraw"));}
+else {alert("error: check capitalization");}
+
+
+
+
+
+
+
+
+
